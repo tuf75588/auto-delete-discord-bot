60c917554c3344b93e06c6ed5931232f42a014e9
Andrew Davis <atd285@gmail.com>
2021-08-22 19:03:35 -0400

    test

--- index.py
+++ index.py
@@ -1,14 +1,17 @@
+
+import os
+
 import discord
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_SECRET')
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    
-
-client.run('')
 
 
-
-
+client.run(TOKEN)
